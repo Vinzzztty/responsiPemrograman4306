9a94749971b13d8655d08e9dc616b8478dc10e10
Vinzzztty <kevinarnandes21@students.amikom.ac.id>
2022-04-19 16:19:57 +0700

    first commit

---
+++ ResponsiPemrograman4306/Karyawan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4306
+{
+    class Karyawan
+    {
+        public void Biodata(string nama, string nim, int gajiBulanan)
+        {
+            Nim = nim;
+            Nama = nama;
+            GajiBulanan = gajiBulanan;
+            //Validasi jika gaji bulanan diisi kurang dari 0
+            if (GajiBulanan < 0)
+            {
+                GajiBulanan = 0; 
+            }
+        }
+
+        //properties
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+        public int GajiBulanan { get; set; } 
+    }
+}
---
+++ ResponsiPemrograman4306/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4306
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int gajik, gajikFix, gajik2, gajikFix2;
+            Karyawan karyawan = new Karyawan();
+            Karyawan karyawan2 = new Karyawan();
+
+            karyawan.Nim = "190302123";
+            karyawan.Nama = "Paijo";
+            karyawan.GajiBulanan = 3000000;
+
+            karyawan2.Nim = "190302124";
+            karyawan2.Nama = "Jono";
+            karyawan2.GajiBulanan = 2000000;
+
+            Console.WriteLine();
+            Console.WriteLine("No Nik/Nama\t\t Gaji Bulanan");
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("1. {0}  {1}\t {2}", karyawan.Nim, karyawan.Nama, karyawan.GajiBulanan);
+            Console.WriteLine("2. {0}  {1}\t {2}", karyawan2.Nim, karyawan2.Nama, karyawan2.GajiBulanan);
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------------");
+            gajik = karyawan.GajiBulanan;
+            gajikFix = gajik + (karyawan.GajiBulanan / 10);
+
+            gajik2 = karyawan2.GajiBulanan;
+            gajikFix2 = gajik + (karyawan2.GajiBulanan / 10);
+
+            Console.WriteLine("Asyikkk kenaikan gaji 10%");
+            Console.WriteLine("1. {0}  {1}\t {2}", karyawan.Nim, karyawan.Nama, gajikFix);
+            Console.WriteLine("2. {0}  {1}\t {2}", karyawan2.Nim, karyawan2.Nama, gajikFix2);
+
+            Console.ReadKey();
+        }
+    }
+}
